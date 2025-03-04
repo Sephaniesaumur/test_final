2e7460615c9d3c9b90cf1048b409ae570f7babe5
Sephaniesaumur <stephanie.denise@saumur.fr>
2025-03-04 15:16:26 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_3704-1",
+      "name": "IMG_3704 (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.6234717395040796,
+        "pitch": -0.03605500697297259,
+        "fov": 1.2763472468432975
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18857914094193973,
+          "pitch": 0.12046562181553178,
+          "rotation": 0,
+          "target": "1-img_3704-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-img_3704-1",
+      "name": "IMG_3704 (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
